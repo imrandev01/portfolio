b57018bebdd23b69748dfd93b6ee23b1ac12342e
imrandev01 <imranainds@gmail.com>
2025-10-10 21:57:37 +0530

    Updated profile picture

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import pic from "../../public/photo2.jpg";
+import pic from "../../public/photo5.jpg";
 
 import { FaSquareFacebook } from "react-icons/fa6";
 import { FaLinkedin } from "react-icons/fa";
